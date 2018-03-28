749272488deaa4b8cc3eb8c7cce3bbdaa16c60cf
Alexander Urizar <urizaraj@gmail.com>
2018-03-28 18:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 function ternaryCheckCity(city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
+
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
+}
