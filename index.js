3b298f856c1603f2497b3b86d53d41501bef66d4
Alexander Urizar <urizaraj@gmail.com>
2018-03-28 18:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ function ternaryCheckCity(city) {
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!'
-  } else if (feet > 2000) {
-    return 'I will gladly take your thirty bucks.'
-  } else if (feet > 2500) {
+  }
+  else if (feet > 2500) {
     return 'No can do.'
+  }
+  else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
   }
 }
